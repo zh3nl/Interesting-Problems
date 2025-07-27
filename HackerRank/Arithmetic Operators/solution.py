9f212396c0b0414d47854dcbeec5183202087f59
Zhen Liu <127721103+zh3nl@users.noreply.github.com>
2025-07-26 23:22:37 -0700

    Create solution.py

---
+++ HackerRank/Arithmetic Operators/solution.py
@@ -0,0 +1,9 @@
+def print_arithmetic(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print_arithmetic(a, b)
