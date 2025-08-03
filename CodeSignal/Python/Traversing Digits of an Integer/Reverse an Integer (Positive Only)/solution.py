545ca77ad57f64b137ea10a606d18681f963e273
Zhen Liu <127721103+zh3nl@users.noreply.github.com>
2025-08-02 22:18:45 -0700

    Create solution.py

---
+++ CodeSignal/Python/Traversing Digits of an Integer/Reverse an Integer (Positive Only)/solution.py
@@ -0,0 +1,9 @@
+def solution(n):
+    # TODO: implement the solution here
+    reverse = 0
+    
+    while n > 0:
+        reverse = (reverse * 10) + (n % 10)
+        n = n // 10
+        
+    return reverse
